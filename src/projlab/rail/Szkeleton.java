6f97672f07c0ac46861b21451297381e493e7f04
Márkus Krisztián <markuskrisztian1@gmail.com>
2017-03-19 20:30:15 +0100

    Skelaton use-case switcher

--- src/projlab/rail/Szkeleton.java
+++ src/projlab/rail/Szkeleton.java
@@ -9,6 +9,37 @@ public class Szkeleton {
 
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    public static int askNumbered(String message, String... options) {
+        if (options == null || options.length == 0) {
+            System.out.println("No possible answers given!");
+            return -1;
+        }
+
+        System.out.println(message);
+        for (int i = 0; i < options.length; i++) {
+            System.out.println(options[i] + " [" + i + "]");
+        }
+
+        String in;
+        try {
+            while ((in = br.readLine()) != null) {
+
+                try {
+                    int n = Integer.parseInt(in);
+                    if (n >= 0 && n < options.length) {
+                        return n;
+                    }
+                } catch (NumberFormatException nfe) {}
+
+                System.out.println("Invalid input!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return -1;
+    }
+
     public static String ask(String message, String... answers) {
         if (answers == null || answers.length == 0) {
             System.out.println("No possible answers given!");
@@ -43,7 +74,39 @@ public class Szkeleton {
     }
 
     public static void main(String[] args) {
+        String[] options = new String[] { "Váltó állítás", "Alagút lerakás", "Vonat léptetés", "Utasok leszállítása, vonat eltűnés és győzelem" };
+        switch (askNumbered("Melyik eset fusson?", options)) {
+            case 0:
+                switchToggle();
+                break;
+            case 1:
+                tunnelPlace();
+                break;
+            case 2:
+                trainStep();
+                break;
+            case 3:
+                passangerUnboard();
+                break;
+            default:
+                break;
+        }
+    }
 
+    private static void switchToggle() {
+        System.out.println("Váltó állítás");
+    }
+
+    private static void tunnelPlace() {
+        System.out.println("Alagút lerakás");
+    }
+
+    private static void trainStep() {
+        System.out.println("Vonat léptetés");
+    }
+
+    private static void passangerUnboard() {
+        System.out.println("Utasok leszállítása, vonat eltűnés és győzelem");
     }
 
 }
