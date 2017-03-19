dcb074af91346afb21a4442612e4e23bd72df355
Márkus Krisztián <markuskrisztian1@gmail.com>
2017-03-19 18:18:40 +0100

    Added new print - Test

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -2,6 +2,7 @@ public class GameEngine {
 
     public static void main(String[] args) {
         System.out.println("Hello ProjektLab asw!");
+        System.out.println("Még valamit kiírok");
     }
 
 }
