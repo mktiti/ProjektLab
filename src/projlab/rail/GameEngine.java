fc671e80dcd085a9b203d5e28df8e7ebdf2de4cd
VM9797 <m.villanyi97@gmail.com>
2017-03-19 18:54:34 +0100

    GE + ME + Loco + Car

---
+++ src/projlab/rail/GameEngine.java
@@ -0,0 +1,48 @@
+package projlab.rail;
+
+import projlab.rail.logic.Locomotive;
+import projlab.rail.logic.MovingEntity;
+import projlab.rail.logic.StaticEntity;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.util.LinkedList;
+
+public class GameEngine {
+
+    private LinkedList<StaticEntity> statics;
+    private LinkedList<Locomotive> locos;
+    //private Tunnel activeTunnelA;
+    //private Tunnel activeTunnelB;
+    //private HiddenRail entryPoint;
+    private StaticEntity last;
+
+    public void connect(MovingEntity first, MovingEntity second){
+        //TODO: Implement this method
+        throw new NotImplementedException();
+    }
+
+    public void load(){
+        //TODO: Implement this method
+        throw new NotImplementedException();
+    }
+
+    public void gameOver(){
+        //TODO: Implement this method
+        throw new NotImplementedException();
+    }
+
+    public void gameWon(){
+        //TODO: Implement this method
+        throw new NotImplementedException();
+    }
+
+    public void setLast(StaticEntity entity){
+        //TODO: Implement this method
+        throw new NotImplementedException();
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
