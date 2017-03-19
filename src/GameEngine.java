977e0cc05c7c49060967c4c91303a4dc2c1e2460
Márkus Krisztián <markuskrisztian1@gmail.com>
2017-03-19 17:23:41 +0100

    Initial Commit

---
+++ src/GameEngine.java
@@ -0,0 +1,11 @@
+/**
+ * Created by titi on 2017.03.19..
+ */
+
+public class GameEngine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello ProjektLab!");
+    }
+
+}
