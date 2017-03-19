eb6a6ab7165f59f810dd74a24ad923900291e7b9
Márkus Krisztián <markuskrisztian1@gmail.com>
2017-03-19 18:24:37 +0100

    Foo created

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,6 +1,10 @@
 public class GameEngine {
 
     public static void main(String[] args) {
+        foo();
+    }
+
+    static void foo() {
         System.out.println("Hello ProjektLab adsf");
     }
 
