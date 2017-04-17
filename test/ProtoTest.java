a0f5e48e57da738b19d698c19a35854ed4b0e585
Márkus Krisztián <markuskrisztian1@gmail.com>
2017-04-17 16:11:13 +0200

    static imports

--- test/ProtoTest.java
+++ test/ProtoTest.java
@@ -5,6 +5,10 @@ import projlab.rail.Proto;
 import projlab.rail.logic.Color;
 import projlab.rail.logic.StaticEntity;
 
+import static projlab.rail.logic.StaticEntity.ConnectionType.*;
+import static projlab.rail.Proto.RailType.*;
+import static projlab.rail.logic.Color.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ProtoTest {
@@ -15,35 +19,35 @@ public class ProtoTest {
     public void initProto() {
         proto = new Proto();
 
-        int prev = createRails(-1, StaticEntity.ConnectionType.A, 10);
+        int prev = createRails(-1, A, 10);
 
-        int s1 = proto.createRail(Proto.RailType.SWITCH);
-        proto.connect(prev, StaticEntity.ConnectionType.A, s1, StaticEntity.ConnectionType.A);
+        int s1 = proto.createRail(SWITCH);
+        proto.connect(prev, A, s1, A);
 
-        int leftLine = createRails(s1, StaticEntity.ConnectionType.B, 4);
-        int station = proto.createStation(Color.RED);
-        proto.connect(leftLine, StaticEntity.ConnectionType.A, station, StaticEntity.ConnectionType.B);
-        leftLine = createRails(station, StaticEntity.ConnectionType.A, 4);
+        int leftLine = createRails(s1, B, 4);
+        int station = proto.createStation(RED);
+        proto.connect(leftLine, A, station, B);
+        leftLine = createRails(station, A, 4);
 
-        int rightLine = createRails(s1, StaticEntity.ConnectionType.IN, 10);
+        int rightLine = createRails(s1, IN, 10);
 
-        int cross = proto.createRail(Proto.RailType.CROSS);
-        proto.connect(leftLine, StaticEntity.ConnectionType.A, cross, StaticEntity.ConnectionType.A);
-        proto.connect(rightLine, StaticEntity.ConnectionType.A, cross, StaticEntity.ConnectionType.X);
+        int cross = proto.createRail(CROSS);
+        proto.connect(leftLine, A, cross, A);
+        proto.connect(rightLine, A, cross, X);
 
-        leftLine = createRails(cross, StaticEntity.ConnectionType.B, 15);
-        int tunnelLeft = proto.createRail(Proto.RailType.TUNNEL);
-        proto.connect(leftLine, StaticEntity.ConnectionType.A, tunnelLeft, StaticEntity.ConnectionType.VISIBLE);
+        leftLine = createRails(cross, B, 15);
+        int tunnelLeft = proto.createRail(TUNNEL);
+        proto.connect(leftLine, A, tunnelLeft, VISIBLE);
 
-        rightLine = createRails(rightLine, StaticEntity.ConnectionType.A, 3);
-        int s2 = proto.createRail(Proto.RailType.SWITCH);
-        proto.connect(rightLine, StaticEntity.ConnectionType.A, s2, StaticEntity.ConnectionType.IN);
+        rightLine = createRails(rightLine, A, 3);
+        int s2 = proto.createRail(SWITCH);
+        proto.connect(rightLine, A, s2, IN);
 
-        int tunnelRight1 = proto.createRail(Proto.RailType.TUNNEL);
-        int tunnelRight2 = proto.createRail(Proto.RailType.TUNNEL);
+        int tunnelRight1 = proto.createRail(TUNNEL);
+        int tunnelRight2 = proto.createRail(TUNNEL);
 
-        proto.connect(s2, StaticEntity.ConnectionType.A, tunnelRight1, StaticEntity.ConnectionType.VISIBLE);
-        proto.connect(s2, StaticEntity.ConnectionType.B, tunnelRight2, StaticEntity.ConnectionType.VISIBLE);
+        proto.connect(s2, A, tunnelRight1, VISIBLE);
+        proto.connect(s2, B, tunnelRight2, VISIBLE);
     }
 
     private int createRails(int toConnect, StaticEntity.ConnectionType connectionType, int number) {
