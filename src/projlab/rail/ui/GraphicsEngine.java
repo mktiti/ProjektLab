1c2f0c98e24cefa5ea06b931f175d79d2a22be02
petib <peti.barton@gmail.com>
2017-05-06 20:26:08 +0200

    Merge branch 'proto' of https://github.com/mktiti/ProjektLab into proto
    # Conflicts:
    #	src/projlab/rail/Interpreter.java
    GraphicsEngine

---
+++ src/projlab/rail/ui/GraphicsEngine.java
@@ -0,0 +1,19 @@
+package projlab.rail.ui;
+
+import projlab.rail.GameEngine;
+import projlab.rail.logic.HiddenRail;
+import projlab.rail.logic.Tunnel;
+
+import java.util.List;
+
+/**
+ * Created by petib on 2017. 05. 06..
+ */
+public class GraphicsEngine {
+
+    private EntityPanel[][] entities;
+    private GameEngine engine;
+
+    public void init(HiddenRail entry, List<Tunnel> tunnels){}
+    public void draw(){}
+}
